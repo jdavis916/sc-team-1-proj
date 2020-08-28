8068238311b0b5d46b6e7ae5becc15a75eabb66a
WebDesignerSteve <47582300+WebDesignerSteve@users.noreply.github.com>
2020-08-27 17:03:43 -0700

    working on js, pushing for merge conflict review

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
 console.log('script connected');
+
+document.getElementsByName('username')
+
+function validate(){
+    if (username.length > 10){
+        alert("Username must be less than 20 characters long, Please Try Again");
+    }
+}
