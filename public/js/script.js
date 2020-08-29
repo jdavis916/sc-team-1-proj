004403d56084f27d2c42b6f3f0b5865d2fc1394f
WebDesignerSteve <47582300+WebDesignerSteve@users.noreply.github.com>
2020-08-28 18:50:26 -0700

    three working dice, stylization not complete

--- public/js/script.js
+++ public/js/script.js
@@ -60,20 +60,22 @@ function checkIfOnlyLetters(field){
 //end of signup page//
 
 //--------------------------------------------------dice------------------------------------------------------------//
+
 function rollDice(){
   var die1 = document.getElementById("die1");
   var die2 = document.getElementById("die2");
+  var die3 = document.getElementById("die3");
   var status = document.getElementById("status");
+  var amtOfDice = document.getElementById("amtOfDice");
+  var sidePerDie = document.getElementById("sidePerDie");
+
   // dice 1-6 //
   var d1 = Math.floor(Math.random() * 6) + 1;
   var d2 = Math.floor(Math.random() * 6) + 1;
-  var diceTotal = d1 + d2;
+  var d3 = Math.floor(Math.random() * 6) + 1;
   die1.innerHTML = d1;
   die2.innerHTML = d2;
-  status.innerHTML = "You rolled "+diceTotal+"."
-
-  // dice 1-20 //
-  // var d1 = Math.floor(Math.random() * 20) + 1; //
+  die3.innerHTML = d3;
 }
 
   //----------------------------------------------end of dice--------------------------------------------//
